1bf52b4147661592fd7a677e7ebdb77b5461d468
okamo <okamo1984@gmail.com>
2023-01-14 17:36:42 +0900

    abc284-f

--- abc284/f/main.go
+++ abc284/f/main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/okamo1984/contest/atcoder"
+)
+
+func solve(n, i int, t string) string {
+	j := n - i
+	s := t[i : len(t)-j]
+	rs := atcoder.Reverse(s)
+	fs := fi(n, i, rs, s)
+	if fs == t {
+		return rs
+	}
+	return ""
+}
+
+func fi(n, i int, s, rs string) string {
+	ri := s[:i]
+	bi := s[i:]
+	return ri + rs + bi
+}
+
+func main() {
+	s := atcoder.NewAtCoderStdInScanner()
+	num := s.ScanNumber()
+	text := s.ScanText()
+	for i := 0; i < num; i++ {
+		answer := solve(num, i, text)
+		if answer != "" {
+			fmt.Println(answer)
+			fmt.Println(i)
+			return
+		}
+	}
+	fmt.Println(-1)
+}
--- atcoder/strings.go
+++ atcoder/strings.go
@@ -15,3 +15,11 @@ func ScanNumberTupleLine(line string) (n, m int) {
 	m, _ = strconv.Atoi(ls.Text())
 	return
 }
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
